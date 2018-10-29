351dcc03b80c833ab5a9c8949890d3cffe87166e
pei.zhang <pei.zhang@7fresh.com>
2018-10-29 16:18:36 +0800

    新增对外Result类.其中定义logKey.用于各系统间联调快速定位请求中所有日志

---
+++ src/main/kotlin/com/annotation/log/logutil/domain/Result.kt
@@ -0,0 +1,32 @@
+package com.annotation.log.logutil.domain
+
+import com.annotation.log.logutil.util.Constants
+import com.annotation.log.logutil.util.LogKey.getThreadKey
+import java.io.Serializable
+
+/**
+ * @ClassName ResultDto
+ * @Description 结果包装
+ * @Author pei.zhang
+ * @Date 2018/10/24
+ * @Version 1.0
+ */
+class Result<T> : Serializable {
+    private val serialVersionUID = 1L
+
+    var isSuccess: Boolean = false
+    var code : Int = Constants.Code.SUCCESS.code
+    var message: String? = null
+    var logId = getThreadKey()
+    var data: T? = null
+
+    constructor(success: Boolean) {
+        this.isSuccess = success
+    }
+
+    constructor(success: Boolean, code: Int, message: String?) {
+        this.code = code
+        this.isSuccess = success
+        this.message = message
+    }
+}
---
+++ src/main/kotlin/com/annotation/log/logutil/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.annotation.log.logutil.util
+
+/**
+ * @ClassName   Constants
+ * @Description TODO
+ * @Author      pei.zhang
+ * @Date        2018/10/25
+ * @Version     1.0
+ */
+class Constants {
+
+    enum class Code(var code : Int, var desc : String) {
+        SUCCESS(10000, "成功");
+    }
+}
